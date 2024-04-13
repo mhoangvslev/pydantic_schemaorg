a4b572c7bf175816577801c9ecb538c49d5eeff8
John Franey <1728528+johnfraney@users.noreply.github.com>
2024-04-13 13:31:39 -0300

    Revert "Rename package"
    This reverts commit d317b09f3e896357892ada2ba4818617ea1e5619.

---
+++ pydantic_schemaorg/Clip.py
@@ -0,0 +1,79 @@
+from __future__ import annotations
+from typing import TYPE_CHECKING
+
+from typing import List, Optional, Union
+from pydantic.v1 import StrictInt, StrictFloat
+
+
+from pydantic.v1 import Field
+from pydantic_schemaorg.CreativeWork import CreativeWork
+
+
+class Clip(CreativeWork):
+    """A short TV or radio program or a segment/part of a program.
+
+    See: https://schema.org/Clip
+    Model depth: 3
+    """
+    type_: str = Field(default="Clip", alias='@type', const=True)
+    musicBy: Optional[Union[List[Union['MusicGroup', 'Person', str]], 'MusicGroup', 'Person', str]] = Field(
+        default=None,
+        description="The composer of the soundtrack.",
+    )
+    partOfSeason: Optional[Union[List[Union['CreativeWorkSeason', str]], 'CreativeWorkSeason', str]] = Field(
+        default=None,
+        description="The season to which this episode belongs.",
+    )
+    director: Optional[Union[List[Union['Person', str]], 'Person', str]] = Field(
+        default=None,
+        description="A director of e.g. TV, radio, movie, video gaming etc. content, or of an event. Directors"
+     "can be associated with individual items or with a series, episode, clip.",
+    )
+    partOfEpisode: Optional[Union[List[Union['Episode', str]], 'Episode', str]] = Field(
+        default=None,
+        description="The episode to which this clip belongs.",
+    )
+    clipNumber: Optional[Union[List[Union[int, 'Integer', str, 'Text']], int, 'Integer', str, 'Text']] = Field(
+        default=None,
+        description="Position of the clip within an ordered group of clips.",
+    )
+    endOffset: Optional[Union[List[Union[StrictInt, StrictFloat, 'Number', 'HyperTocEntry', str]], StrictInt, StrictFloat, 'Number', 'HyperTocEntry', str]] = Field(
+        default=None,
+        description="The end time of the clip expressed as the number of seconds from the beginning of the work.",
+    )
+    actor: Optional[Union[List[Union['Person', str]], 'Person', str]] = Field(
+        default=None,
+        description="An actor, e.g. in TV, radio, movie, video games etc., or in an event. Actors can be associated"
+     "with individual items or with a series, episode, clip.",
+    )
+    partOfSeries: Optional[Union[List[Union['CreativeWorkSeries', str]], 'CreativeWorkSeries', str]] = Field(
+        default=None,
+        description="The series to which this episode or season belongs.",
+    )
+    directors: Optional[Union[List[Union['Person', str]], 'Person', str]] = Field(
+        default=None,
+        description="A director of e.g. TV, radio, movie, video games etc. content. Directors can be associated"
+     "with individual items or with a series, episode, clip.",
+    )
+    actors: Optional[Union[List[Union['Person', str]], 'Person', str]] = Field(
+        default=None,
+        description="An actor, e.g. in TV, radio, movie, video games etc. Actors can be associated with individual"
+     "items or with a series, episode, clip.",
+    )
+    startOffset: Optional[Union[List[Union[StrictInt, StrictFloat, 'Number', 'HyperTocEntry', str]], StrictInt, StrictFloat, 'Number', 'HyperTocEntry', str]] = Field(
+        default=None,
+        description="The start time of the clip expressed as the number of seconds from the beginning of the"
+     "work.",
+    )
+    
+
+if TYPE_CHECKING:
+    from pydantic_schemaorg.MusicGroup import MusicGroup
+    from pydantic_schemaorg.Person import Person
+    from pydantic_schemaorg.CreativeWorkSeason import CreativeWorkSeason
+    from pydantic_schemaorg.Episode import Episode
+    from pydantic_schemaorg.Integer import Integer
+    from pydantic_schemaorg.Text import Text
+    from pydantic_schemaorg.Number import Number
+    from pydantic_schemaorg.HyperTocEntry import HyperTocEntry
+    from pydantic_schemaorg.CreativeWorkSeries import CreativeWorkSeries
---
+++ pydantic_schemaorg/DietNutrition.py
@@ -0,0 +1,16 @@
+from __future__ import annotations
+
+
+from pydantic.v1 import Field
+from pydantic_schemaorg.MedicalSpecialty import MedicalSpecialty
+from pydantic_schemaorg.MedicalBusiness import MedicalBusiness
+
+
+class DietNutrition(MedicalSpecialty, MedicalBusiness):
+    """Dietetics and nutrition as a medical specialty.
+
+    See: https://schema.org/DietNutrition
+    Model depth: 5
+    """
+    type_: str = Field(default="DietNutrition", alias='@type', const=True)
+    
